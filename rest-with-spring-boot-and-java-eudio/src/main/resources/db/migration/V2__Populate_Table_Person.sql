1ba13de4450865a61a36ee5397928760484aa7a7
KaiqueLusvarghi <kaiqueolusvarghi@gmail.com>
2023-10-31 20:21:49 -0300

    First Commit

---
+++ rest-with-spring-boot-and-java-eudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Praia Grande', 'Kaique', 'Male', 'Ortolani'),
+	(2, 'Praia Grande', 'Gabriela', 'Female', 'Santos'),
+	(5, 'Miami - US', 'Kauã', 'Male', 'Lusvarghi '),
+	(7, 'Suiça ', 'Jubileu', 'Male', 'Wosvaldo'),
+	(8, 'Smiljan - Croatia ', 'Nikola', 'Male', 'Tesla');
